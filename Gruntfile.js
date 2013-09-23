c497b4d09c1a196e542b9c983c69f97c46634edd
asciidisco <public@asciidisco.com>
2013-09-23 18:38:10 +0200

    Updating Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,7 +140,8 @@ module.exports = function(grunt) {
         createTag: true,
         tagName: '%VERSION%',
         tagMessage: '%VERSION%',
-        push: true
+        push: true,
+        pushTo: 'git@github.com:dalekjs/grunt-dalek.git'
       }
     },
 
